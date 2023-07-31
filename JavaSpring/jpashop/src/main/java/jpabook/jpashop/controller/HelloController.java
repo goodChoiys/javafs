176dd1d0612021f6b845ea2708a7bc51be13c64e
chois1234 <choiys9120@gmail.com>
2023-07-31 18:15:41 +0900

    0731

---
+++ JavaSpring/jpashop/src/main/java/jpabook/jpashop/controller/HelloController.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping ("/")
+    public String hello(){
+        return "index";
+    }
+
+
+    @GetMapping ("/hi")
+    public String hello(Model model){
+        model.addAttribute("data","Hello!");
+        return "hello";
+    }
+
+    @GetMapping ("/hello")
+    public String hello2(Model model){
+        model.addAttribute("name","홍길동");
+        model.addAttribute("data","커피");
+
+        return "hello2";
+    }
+
+}
