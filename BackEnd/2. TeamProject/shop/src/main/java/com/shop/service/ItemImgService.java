810b4090a2cf31c53a3726971c35f1a76c0a814e
chois1234 <choiys9120@gmail.com>
2023-08-29 18:06:59 +0900

    0829

--- BackEnd/2. TeamProject/shop/src/main/java/com/shop/service/ItemImgService.java
+++ BackEnd/2. TeamProject/shop/src/main/java/com/shop/service/ItemImgService.java
@@ -3,7 +3,6 @@ package com.shop.service;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
